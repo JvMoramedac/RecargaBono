e36620bd3b3c817c11683c7ee04a691bf93a470b
Javier <fml0017@aliu.medac.es>
2025-10-07 14:06:15 +0200

    recargatren

--- src/main/java/Recargatren.java
+++ src/main/java/Recargatren.java
@@ -1,2 +1,19 @@
+import javax.swing.*;
+
 public class Recargatren {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Recargatren");
+        frame.setContentPane(new Recargatren().RECARGATREN);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private JPanel RECARGATREN;
+    private JButton bono10Viajes12Button;
+    private JButton bonoTrimestral12000€Button;
+    private JButton PAGARButton;
+    private JButton VOLVERButton;
+    private JComboBox comboBox1;
+
 }
